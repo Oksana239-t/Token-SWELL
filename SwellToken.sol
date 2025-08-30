a4b1900d346dab58924190cba028b0997adf65d7
Oksana239-t <kovaloksana609@gmail.com>
2025-08-30 17:19:21 +0200

    SwellToken.sol

--- SwellToken.sol
+++ SwellToken.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // ← правильний імпорт
+
+contract SwellToken is ERC20("Swell Governance Token", "SWELL") {
+    constructor(address receiver, uint256 totalSupply) {
+        _mint(receiver, totalSupply);
+    }
+}
